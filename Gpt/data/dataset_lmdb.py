2fc7c6a8581109e82d472c30d65f9d25dfb98200
cubist38 <74866985+cubist38@users.noreply.github.com>
2022-10-08 20:25:26 +0700

    a

--- Gpt/data/dataset_lmdb.py
+++ Gpt/data/dataset_lmdb.py
@@ -24,7 +24,7 @@ class ParameterDataset(Dataset):
     dataset_name: str = "mnist_loss"                # Name of the dataset in tasks.py
     split: str = "train"                            # Split of the checkpoint dataset to use ("train" or "test")
     max_train_runs: int = 1000000                   # Maximum number of runs to train on (default: all)
-    num_test_runs: int = 0 #500                      # Number of runs in the test split
+    num_test_runs: int = 500 #500                      # Number of runs in the test split
     target_epoch_size: int = 806400                 # Amount of data to train on per-"epoch" (806400 is arbitrary)
     train_metric: str = "avg_test_loss"             # Conditional metric for G.pt
     min_step_spacing: int = 1                       # Minimum spacing between starting and future checkpoints
